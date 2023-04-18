6fa35d160cc7a27a5cb43dfa3efe087c5d40b44a
caldeira54 <caldeiraa54@gmail.com>
2023-04-17 23:45:56 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import Cabecalho from './componentes/cabecalho/cabecalho'
 function App() {
 
   return (
-    <div className="container">
+    <>
       <Cabecalho />
-    </div>
+
+      <div className='container'>
+        <div className='img'>
+          <img src="./assets/rosto.jpeg" alt="ROSTO" />
+        </div>
+
+        <div className='nome'>
+          <h2>Arthur Caldeira</h2>
+        </div>
+
+        <div className='redes-sociais'>
+          <a href="https://www.linkedin.com/in/arthur-alves-caldeira-67b708228/">Linkedin</a>
+          <a href="https://github.com/caldeira54?tab=repositories">GitHub</a>
+        </div>
+      </div>
+    </>
   )
 }
 
