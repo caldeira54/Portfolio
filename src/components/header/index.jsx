b2e243411f141db220fd7aa547542994733eb44c
caldeira54 <caldeiraa54@gmail.com>
2023-04-25 20:01:44 -0300

    Ajustes

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,27 @@
-import './header.css' ;
+import { useNavigate } from 'react-router-dom';
+
+import './header.css';
 
 export default function Header() {
+    const navigate = useNavigate();
+
+    function aboutMe() {
+        return navigate('../../pages/aboutme');
+    }
+
+    function portfolio() {
+        return navigate('../../pages/portfolio');
+    }
+
     return (
         <div className='container'>
-            <div className='nome'>
+            <div className='name'>
                 <small>Arthur Caldeira</small>
             </div>
 
             <div className='menu'>
-                <a href="#">Sobre mim</a>
-                <a href="#">Portfolio</a>
+                <a onClick={aboutMe}>Sobre mim</a>
+                <a onClick={portfolio}>Portfolio</a>
             </div>
         </div>
     );
